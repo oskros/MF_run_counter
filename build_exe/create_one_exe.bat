0339585d99119b9262b9b3b2035a3ef8fe42f690
oskros <53574923+oskros@users.noreply.github.com>
2019-08-20 23:40:46 +0200

    UI showcase update

--- build_exe/create_one_exe.bat
+++ build_exe/create_one_exe.bat
@@ -5,4 +5,6 @@ cd "C:\Users\oskro\PycharmProjects\MF_counter_releases"
 path=C:\Users\oskro\Downloads\WPy-3701\python-3.7.0.amd64;C:\Users\oskro\Downloads\WPy-3701\python-3.7.0.amd64\Scripts
 pyinstaller "C:\Users\oskro\PycharmProjects\MF_counter_releases\build_exe\mf_timer_onefile.spec" --windowed --onefile --clean --icon="C:\Users\oskro\PycharmProjects\MF_counter_releases\media\icon.ico"
 start C:\Users\oskro\PycharmProjects\MF_counter_releases\release
+rmdir /S /Q "C:\Users\oskro\PycharmProjects\MF_counter_releases\build"
+rmdir /S /Q "C:\Users\oskro\PycharmProjects\MF_counter_releases\dist"
 exit
