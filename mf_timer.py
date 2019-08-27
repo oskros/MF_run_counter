3b845cd98b65a4d3b40bcb0950368aabd2c07512
Oskros <oskros@outlook.dk>
2019-08-27 13:04:53 +0200

    add comments and reformat some functions

--- mf_timer.py
+++ mf_timer.py
@@ -28,7 +28,7 @@ class MFRunTimer(tk.Frame):
         self.main_frame = main_frame
         self._start = 0.0
         self._session_start = time.time()
-        self._sessiontime = 0.0
+        self.session_time = 0.0
         self._laptime = 0.0
         self._running = False
         self._paused = False
@@ -50,7 +50,7 @@ class MFRunTimer(tk.Frame):
         self.c1, self.circ_id = tk_utils.add_circle(flt, 14, 'red')
         self.c1.grid(row=0, column=0, padx=3, pady=3)
         l0 = tk.Label(flt, textvariable=self.sessionstr, font=('arial', 10))
-        self._set_time(self._sessiontime, for_session=True)
+        self._set_time(self.session_time, for_session=True)
         l0.grid(row=0, column=1, sticky=tk.N, padx=20)
 
         l1 = tk.Label(self, textvariable=self.timestr, font='arial 20')
@@ -94,8 +94,8 @@ class MFRunTimer(tk.Frame):
         self._timer = self.after(50, self._update_lap_time)
 
     def _update_session_time(self):
-        self._sessiontime = time.time() - self._session_start
-        self._set_time(self._sessiontime, for_session=True)
+        self.session_time = time.time() - self._session_start
+        self._set_time(self.session_time, for_session=True)
         self._sess_timer = self.after(50, self._update_session_time)
 
     exec(blocks[6])
@@ -129,14 +129,14 @@ class MFRunTimer(tk.Frame):
     def load_from_state(self, state):
         self.laps = []
         self.m.delete(0, tk.END)
-        self._sessiontime = state.get('session_time', 0)
-        self._session_start = time.time() - self._sessiontime
+        self.session_time = state.get('session_time', 0)
+        self._session_start = time.time() - self.session_time
         for lap in state.get('laps', []):
             self.Lap(lap, force=True)
         self._set_laps(is_running=False)
         self._set_fastest()
         self._set_average()
-        self._set_time(self._sessiontime, for_session=True)
+        self._set_time(self.session_time, for_session=True)
 
     def Start(self, play_sound=True):
         def update_start():
@@ -200,7 +200,7 @@ class MFRunTimer(tk.Frame):
 
             self.c1.itemconfigure(self.circ_id, fill='red')
             self._set_time(self._laptime, for_session=False)
-            self._set_time(self._sessiontime, for_session=True)
+            self._set_time(self.session_time, for_session=True)
             if self._running:
                 self.after_cancel(self._timer)
             self.after_cancel(self._sess_timer)
@@ -209,7 +209,7 @@ class MFRunTimer(tk.Frame):
         else:
             self.pause_lab.destroy()
             self._start = time.time() - self._laptime
-            self._session_start = time.time() - self._sessiontime
+            self._session_start = time.time() - self.session_time
             if self._running:
                 self.c1.itemconfigure(self.circ_id, fill='green3')
                 self._update_lap_time()
@@ -228,18 +228,18 @@ class MFRunTimer(tk.Frame):
             self.Stop()
         self._start = time.time()
         self._laptime = 0.0
-        self._sessiontime = 0.0
+        self.session_time = 0.0
         self._session_start = time.time()
         self.laps = []
         self.m.delete(0, tk.END)
         self._set_time(self._laptime, for_session=False)
-        self._set_time(self._sessiontime, for_session=True)
+        self._set_time(self.session_time, for_session=True)
         self._set_laps(is_running=self._running)
         self._set_fastest()
         self._set_average()
 
     def SaveState(self):
-        return dict(laps=self.laps, session_time=self._sessiontime)
+        return dict(laps=self.laps, session_time=self.session_time)
 
 
 class Drops(tk.Frame):
@@ -401,19 +401,24 @@ class Profile(tk.Frame):
     def _delete_profile(self):
         chosen = self.profile_dropdown.get()
         if chosen == '':
+            # If nothing is selected the function returns
             return
         if len(self.profile_dropdown['values']) <= 1:
             tk.messagebox.showerror('Error', 'You need to have at least one profile, create a new profile before deleting this one.')
             return
+
+        # Open window at the center of the application
         xc = self.root.winfo_rootx() + self.root.winfo_width()//8
         yc = self.root.winfo_rooty() + self.root.winfo_height()//3
         resp1 = tk_utils.mbox(msg='Are you sure you want to delete this profile? This will permanently delete all records stored for the profile.', title='WARNING', coords=(xc, yc))
         if resp1 is True:
             resp2 = tk_utils.mbox(msg='Are you really really sure you want to delete the profile? Final warning!', b1='Cancel', b2='OK', title='WARNING', coords=(xc,yc))
-            if resp2 is False:
+            if resp2 is False:  # False here because we switch buttons around in second confirmation
                 file = 'Profiles/%s.json' % chosen
                 os.remove(file)
                 self.main_frame.profiles.remove(chosen)
+
+                # We change active profile to an existing profile
                 self.main_frame.active_profile = self.main_frame.profiles[0]
                 self.active_profile.set(self.main_frame.profiles[0])
                 self.profile_dropdown['values'] = self.main_frame.profiles
@@ -422,24 +427,32 @@ class Profile(tk.Frame):
     def delete_archived_session(self):
         chosen = self.archive_dropdown.get()
         if chosen == '':
+            # If nothing is selected the function returns
             return
         if chosen == 'Profile history':
             tk.messagebox.showerror('Error', 'You cannot delete profile history from here. Please delete all sessions manually, or delete the profile instead')
             return
+
+        # Open window at the center of the application
         xc = self.root.winfo_rootx() + self.root.winfo_width()//8
         yc = self.root.winfo_rooty() + self.root.winfo_height()//3
         resp = tk_utils.mbox(msg='Do you really want to delete this session from archive? It will be permanently deleted', title='WARNING', coords=(xc, yc))
         if resp:
             if chosen == 'Active session':
+                # Here we simply reset the timer module
                 self.main_frame.ResetSession()
                 self.main_frame.SaveActiveState()
                 self.selected_archive.set('Active session')
                 return
+
+            # Load the profile .json, delete the selected session and save the modified dictionary back to the .json
             cache = self.main_frame.load_state_file()
             cache.pop(chosen, None)
             file = 'Profiles/%s.json' % self.active_profile.get()
             with open(file, 'w') as fo:
                 json.dump(cache, fo, indent=2)
+
+            # Update archive dropdown and descriptive statistics
             self.available_archive.remove(chosen)
             self.archive_dropdown['values'] = self.available_archive
             self.selected_archive.set('Active session')
@@ -450,19 +463,25 @@ class Profile(tk.Frame):
         laps = []
         session_time = 0
         dropcount = 0
+        # Concatenate information from each available session
         for key in [x for x in active.keys() if x not in ['active_state', 'extra_data']]:
             laps.extend(active[key].get('laps', []))
             session_time += active[key].get('session_time', 0)
             drops = active[key].get('drops', dict())
             for drop, val in drops.items():
                 dropcount += len(val)
+
+        # Append data for active session from timer module
         laps.extend(self.main_frame.tab1.laps)
-        session_time += self.main_frame.tab1._sessiontime
+        session_time += self.main_frame.tab1.session_time
         for drop, val in self.main_frame.tab2.drops.items():
             dropcount += len(val)
+
+        # Ensure no division by zero errors by defaulting to displaying 0
         avg_lap = sum(laps) / len(laps) if laps else 0
         pct = sum(laps) * 100 / session_time if session_time > 0 else 0
 
+        # (re-)Populate the listbox with descriptive statistics
         self.descr.delete(0, tk.END)
         self.descr.insert(tk.END, 'Total session time:   ' + tk_utils.build_time_str(session_time))
         self.descr.insert(tk.END, 'Total run time:       ' + tk_utils.build_time_str(sum(laps)))
@@ -475,7 +494,10 @@ class Profile(tk.Frame):
     def open_archive_browser(self):
         chosen = self.archive_dropdown.get()
         if chosen == '':
+            # If nothing is selected the function returns
             return
+
+        # We build the new tkinter window to be opened
         new_win = tk.Toplevel()
         new_win.title('Archive browser')
         new_win.wm_attributes('-topmost', 1)
@@ -483,60 +505,73 @@ class Profile(tk.Frame):
         new_win.geometry('+%d+%d' % (self.main_frame.root.winfo_rootx(), self.main_frame.root.winfo_rooty()))
         new_win.focus_get()
         new_win.iconbitmap(os.path.join(getattr(sys, '_MEIPASS', os.path.abspath('.')), frozen + 'icon.ico'))
+        title = tk.Label(new_win, text='Archive browser', font='Helvetica 14')
 
-        header = tk.Label(new_win, text='Archive browser', font='Helvetica 14')
-
+        # Handle how loading of session data should be treated in the 3 different cases
         if chosen == 'Active session':
-            session_time = self.main_frame.tab1._sessiontime
+            # Load directly from timer module
+            session_time = self.main_frame.tab1.session_time
             laps = self.main_frame.tab1.laps
             drops = self.main_frame.tab2.drops
         elif chosen == 'Profile history':
+            # Load everything from profile .json, and append data from timer module
             active = self.main_frame.load_state_file()
             laps = []
             session_time = 0
             drops = dict()
+            # Concatenate information from each available session
             for key in [x for x in active.keys() if x not in ['active_state', 'extra_data']]:
                 session_drops = active[key].get('drops', dict())
-                for d, val in session_drops.items():
-                    drops[str(int(d)+len(laps))] = val
+                for run_no, run_drop in session_drops.items():
+                    drops[str(int(run_no)+len(laps))] = run_drop
                 laps.extend(active[key].get('laps', []))
                 session_time += active[key].get('session_time', 0)
-            for d, val in self.main_frame.tab2.drops.items():
-                drops[str(int(d) + len(laps))] = val
+
+            # Append data for active session from timer module
+            for run_no, run_drop in self.main_frame.tab2.drops.items():
+                drops[str(int(run_no) + len(laps))] = run_drop
             laps.extend(self.main_frame.tab1.laps)
-            session_time += self.main_frame.tab1._sessiontime
+            session_time += self.main_frame.tab1.session_time
         else:
+            # Load selected session data from profile .json
             active = self.main_frame.load_state_file()
             chosen_archive = active.get(chosen, dict())
             session_time = chosen_archive.get('session_time', 0)
             laps = chosen_archive.get('laps', [])
             drops = chosen_archive.get('drops', dict())
+
+        # Ensure no division by zero errors by defaulting to displaying 0
         avg_lap = sum(laps) / len(laps) if laps else 0
         pct = sum(laps) * 100 / session_time if session_time > 0 else 0
 
-        sbfr = tk.Frame(new_win)
-        vscroll = tk.Scrollbar(sbfr, orient=tk.VERTICAL)
+        # Configure the list frame with scrollbars which displays the archive of the chosen session
+        list_frame = tk.Frame(new_win)
+        vscroll = tk.Scrollbar(list_frame, orient=tk.VERTICAL)
         hscroll = tk.Scrollbar(new_win, orient=tk.HORIZONTAL)
-        m = tk.Listbox(sbfr, selectmode=tk.EXTENDED, yscrollcommand=vscroll.set, xscrollcommand=hscroll.set, activestyle='none')
-        m.bind('<FocusOut>', lambda e: m.selection_clear(0, tk.END))
-        m.config(font=('courier', 10))
-        hscroll.config(command=m.xview)
-        vscroll.config(command=m.yview)
+        txt_list = tk.Listbox(list_frame, selectmode=tk.EXTENDED, yscrollcommand=vscroll.set, xscrollcommand=hscroll.set, activestyle='none', font=('courier', 10))
+        txt_list.bind('<FocusOut>', lambda e: txt_list.selection_clear(0, tk.END))  # Lose selection when shifting focus
+        txt_list.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        hscroll.config(command=txt_list.xview)
+        vscroll.config(command=txt_list.yview)
+        vscroll.pack(side=tk.LEFT, fill=tk.Y)
 
-        output = list()
-        output.append(['Character name: ', self.extra_data.get('Character name', '')])
-        output.append(['Run type: ', self.extra_data.get('Run type', '')])
-        output.append(['Active MF %: ', self.extra_data.get('Active MF %', '')])
-        output.append([''])
-        output.append(['Total session time:   ', tk_utils.build_time_str(session_time)])
-        output.append(['Total run time:       ', tk_utils.build_time_str(sum(laps))])
-        output.append(['Average run time:     ', tk_utils.build_time_str(avg_lap)])
-        output.append(['Fastest run time:     ', tk_utils.build_time_str(min(laps, default=0))])
-        output.append(['Time spent in runs: ', str(round(pct, 2)) + '%'])
-        output.append([''])
+        # Build header for output file with information and descriptive statistics
+        output = [['Character name: ', self.extra_data.get('Character name', '')],
+                  ['Run type: ', self.extra_data.get('Run type', '')],
+                  ['Active MF %: ', self.extra_data.get('Active MF %', '')],
+                  [''],
+                  ['Total session time:   ', tk_utils.build_time_str(session_time)],
+                  ['Total run time:       ', tk_utils.build_time_str(sum(laps))],
+                  ['Average run time:     ', tk_utils.build_time_str(avg_lap)],
+                  ['Fastest run time:     ', tk_utils.build_time_str(min(laps, default=0))],
+                  ['Time spent in runs: ', str(round(pct, 2)) + '%'],
+                  ['']]
 
+        # If drops were added before first run is started, we make sure to include them in output anyway
         if '0' in drops.keys():
-            output.append(['Run 0: ', *drops['0']])
+            output.append(['Run 0: ', 'NO_TIME', *drops['0']])
+
+        # Loop through all runs and add run times and drops for each run
         for n, lap in enumerate(laps, 1):
             str_n = ' ' * max(len(str(len(laps))) - len(str(n)), 0) + str(n)
             droplst = drops.get(str(n), [])
@@ -545,34 +580,40 @@ class Profile(tk.Frame):
                 tmp += droplst
             output.append(tmp)
 
+        # Format string list to be shown in the archive browser
         for op in output:
             tmpstr = ''.join(op[:2])
             if len(op) > 2:
                 tmpstr += ' --- ' + ', '.join(op[2:])
-            m.insert(tk.END, tmpstr)
+            txt_list.insert(tk.END, tmpstr)
 
-        fr = tk.Frame(new_win)
-        b1 = tk.Button(fr, text='Copy to clipboard', command=lambda: self.copy_to_clipboard(new_win, '\n'.join(m.get(0, tk.END))))
-        b2 = tk.Button(fr, text='Save as .txt', command=lambda: self.save_to_txt('\n'.join(m.get(0, tk.END))))
-        b3 = tk.Button(fr, text='Save as .csv', command=lambda: self.save_to_csv(output))
+        button_frame = tk.Frame(new_win)
+        tk.Button(button_frame, text='Copy to clipboard', command=lambda: self.copy_to_clipboard(new_win, '\n'.join(txt_list.get(0, tk.END)))).pack(side=tk.LEFT, fill=tk.X)
+        tk.Button(button_frame, text='Save as .txt', command=lambda: self.save_to_txt('\n'.join(txt_list.get(0, tk.END)))).pack(side=tk.LEFT, fill=tk.X)
+        tk.Button(button_frame, text='Save as .csv', command=lambda: self.save_to_csv(output)).pack(side=tk.LEFT, fill=tk.X)
 
-        header.pack(side=tk.TOP)
-        fr.pack(side=tk.BOTTOM)
-        b1.pack(side=tk.LEFT, fill=tk.X)
-        b2.pack(side=tk.LEFT, fill=tk.X)
-        b3.pack(side=tk.LEFT, fill=tk.X)
-        sbfr.pack(fill=tk.BOTH, expand=1)
-        m.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
-        vscroll.pack(side=tk.LEFT, fill=tk.Y)
+        # Packs all the buttons and UI in the archive browser. Packing order is very important:
+        # TOP: Title first (furthest up), then list frame
+        # BOTTOM: Buttons first (furthest down) and then horizontal scrollbar
+        title.pack(side=tk.TOP)
+        list_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        button_frame.pack(side=tk.BOTTOM)
         hscroll.pack(side=tk.BOTTOM, fill=tk.X)
 
     @staticmethod
     def copy_to_clipboard(obj, string):
+        """
+        Clears current clipboard and adds the string instead
+        """
         obj.clipboard_clear()
         obj.clipboard_append(string)
 
     @staticmethod
     def save_to_txt(string):
+        """
+        Writes a string to text file. Adds a line break every time '\n' is encountered in the string.
+        'asksaveasfile' returns a writable object that then passes information on to the .txt file
+        """
         f = tk.filedialog.asksaveasfile(mode='w', defaultextension='.txt', filetypes=(('.txt', '*.txt'), ('All Files', '*.*')))
         if not f:
             return
@@ -580,13 +621,19 @@ class Profile(tk.Frame):
         f.close()
 
     @staticmethod
-    def save_to_csv(string):
+    def save_to_csv(string_lst):
+        """
+        Writes a list of lists of strings to a .csv file
+
+        Here we use asksaveasfilename in order to just return a path instead of writable object, because then we can
+        initiate our own csv writer opbject with the newline='' option, which ensures we don't have double line breaks
+        """
         f = tk.filedialog.asksaveasfilename(defaultextension='.csv', filetypes=(('.csv', '*.csv'), ('All Files', '*.*')))
         if not f:
             return
         with open(f, newline='', mode='w') as fo:
             writer = csv.writer(fo, delimiter=',')
-            writer.writerows(string)
+            writer.writerows(string_lst)
 
 
 class About(tk.Frame):
