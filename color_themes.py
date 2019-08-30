30d04208d8f5ae9c033e527ff97bfb49cc22bef9
Oskros <oskros@outlook.dk>
2019-08-30 13:24:09 +0200

    added a blue theme and upgraded colors a little

--- color_themes.py
+++ color_themes.py
@@ -1,30 +1,34 @@
 from tkinter import ttk
-available_themes = ['ugly_blue', 'dark', 'default']
+available_themes = ['blue', 'dark', 'default']
 
 
 class Theme:
     def __init__(self, used_theme):
-        if used_theme == 'ugly_blue':
+        if used_theme == 'blue':
             # General
-            default_color = 'light sky blue'
+            default_color = 'light blue'
             self.ttk_style = 'clam'
 
             # Backgrounds
             self.frame_color = default_color
             self.border_color = default_color
             self.label_color = default_color
-            self.notebook_background = default_color
+
+            # Tab colors
+            self.notebook_background_color = default_color
+            self.selected_tab_color = 'alice blue'
+            self.hover_tab_background_color = 'lavender'
 
             # Widgets
-            self.button_color = '#40ccd0'
+            self.button_color = 'light sky blue'
             self.activebutton_color = 'white'
             self.listbox_color = 'white'
-            self.pause_button_color = 'deep sky blue'
+            self.pause_button_color = 'DodgerBlue2'
             self.circle_border_color = 'black'
             self.entry_color = 'white'
 
             # Text
-            self.text_color = '#ffffff'
+            self.text_color = 'black'
             self.run_count_color = 'red'
             self.hyperlink_color = 'blue'
             self.listbox_text = 'black'
@@ -32,8 +36,8 @@ class Theme:
 
             # Selecting Comboboxes
             self.combohighlight_color = 'black'  # Highlight selection color
-            self.combofield_color = 'SteelBlue1'  # Background of the combobox
-            self.dropdown_button_color = 'blue'  # Color of the dropdown button
+            self.combofield_color = 'lavender'  # Background of the combobox
+            self.dropdown_button_color = 'cornflower blue'  # Color of the dropdown button
             self.combo_listbox_foreground = 'black'
             self.combo_listbox_selectbackground = 'black'
             self.combo_listbox_selectforeground = 'white'
@@ -46,7 +50,11 @@ class Theme:
             self.frame_color = default_color
             self.label_color = default_color
             self.border_color = 'dark grey'
-            self.notebook_background = 'grey10'
+
+            # Tab colors
+            self.notebook_background_color = 'grey10'
+            self.selected_tab_color = 'grey20'
+            self.hover_tab_background_color = 'grey30'
 
             # Widgets
             self.button_color = 'grey40'
@@ -79,7 +87,11 @@ class Theme:
             self.frame_color = default_color
             self.border_color = default_color
             self.label_color = default_color
-            self.notebook_background = default_color
+
+            # Tab colors
+            self.notebook_background_color = default_color
+            self.selected_tab_color = 'white'
+            self.hover_tab_background_color = 'white'
 
             # Widgets
             self.button_color = default_color
@@ -108,6 +120,15 @@ class Theme:
 
     def apply_theme_style(self):
         style = ttk.Style()
+        # style.theme_create('combostyle', parent=self.ttk_style,
+        #                    settings={'TCombobox':
+        #                                  {'configure':
+        #                                       {'selectbackground': self.combohighlight_color,
+        #                                        # 'selectforeground': 'grey90',
+        #                                        'fieldbackground': self.combofield_color,
+        #                                        'background': self.dropdown_button_color,
+        #                                        }}}
+        #                    )
         style.theme_create('combostyle', parent=self.ttk_style,
                            settings={'TCombobox':
                                          {'configure':
@@ -115,8 +136,14 @@ class Theme:
                                                # 'selectforeground': 'grey90',
                                                'fieldbackground': self.combofield_color,
                                                'background': self.dropdown_button_color,
-                                               }}}
+                                               }},
+                                     "TNotebook.Tab": {
+                                         "configure": {"padding": [5, 1], "background": self.frame_color},
+                                         "map": {"background": [("selected", self.selected_tab_color), ("active", self.hover_tab_background_color)],
+                                                 "expand": [("selected", [2, 1, 2, 0])]}}
+                                     }
                            )
+
         style.theme_use('combostyle')
         style.element_create('Plain.Notebook.tab', "from", self.ttk_style)
         style.element_create('Plain.Notebook', "from", self.ttk_style)
