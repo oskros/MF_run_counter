30c461397b33716d3c19591219696c264d2b4c7d
oskro <oskro@orsted.com>
2023-10-02 12:27:10 +0200

    mutex was path specific, creating possibility to run duplicates from different paths

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ try:
     import logging
     import sys
     import os
+    from tkinter import messagebox
     from utils import tk_utils
     from master_frame import MasterFrame
 
-    mp = os.path.abspath(__file__).replace('\\', '/') + "_{A957B84C-9800-4BAE-8B56-8F446AB8ED5B}"
-    mutex = win32event.CreateMutex(None, False, mp)
+    mutex = win32event.CreateMutex(None, False, 'A957B84C-9800-4BAE-8B56-8F446AB8ED5B')
     last_error = win32api.GetLastError()
     if last_error == ERROR_ALREADY_EXISTS:
-        logging.exception(f'Application is already open. Aborting. {last_error}')
+        messagebox.showerror('Already open', 'An instance of MF Run Counter is already open. Aborting')
+        logging.exception(f'An instance of MF Run Counter is already open. Aborting. {last_error}')
         sys.exit(0)
 
     MasterFrame()
