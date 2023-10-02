1c2d6735a37c84a9f9a94029002fe79b5bba5674
oskro <oskro@orsted.com>
2023-10-02 10:10:11 +0200

    use mutex to check multiple instances

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ try:
     from winerror import ERROR_ALREADY_EXISTS
     import logging
     import sys
+    import os
     from utils import tk_utils
     from master_frame import MasterFrame
 
-    mutex = win32event.CreateMutex(None, False, 'name')
+    mp = os.path.abspath(__file__).replace('\\', '/') + "_{MF RUN COUNTER}"
+    mutex = win32event.CreateMutex(None, False, mp)
     last_error = win32api.GetLastError()
     if last_error == ERROR_ALREADY_EXISTS:
         logging.exception(f'Application is already open. Aborting. {last_error}')
