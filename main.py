d6db9fdfcbb03615f9439b495cfc95722fecf0c9
oskro <oskro@orsted.com>
2023-11-03 10:31:39 +0100

    refactoring

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ try:
     mutex = win32event.CreateMutex(None, False, 'A957B84C-9800-4BAE-8B56-8F446AB8ED5B')
     last_error = win32api.GetLastError()
     if last_error == ERROR_ALREADY_EXISTS:
-        messagebox.showerror('Already open', 'An instance of MF Run Counter is already open. Aborting')
-        logging.exception(f'An instance of MF Run Counter is already open. Aborting. {last_error}')
+        messagebox.showerror('Already open', 'An instance of MF Run Counter is already open - Aborting!')
+        logging.exception(f'An instance of MF Run Counter is already open - Aborting! {last_error}')
         sys.exit(0)
 
     MasterFrame()
