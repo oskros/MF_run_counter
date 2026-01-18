26657dab7644878753c0c25326ceb1f811efa061
oskros <53574923+oskros@users.noreply.github.com>
2026-01-18 21:00:58 +0100

    * refactor "media" to "assets" * simplified the themes class, added a theme_colors file

--- init.py
+++ init.py
@@ -2,7 +2,7 @@ import sys
 import os
 version = '1.6.0'
 
-# The exe files do not have a assets folder, so the assets folder should only be added to the path when code is run
+# The exe files do not have an assets folder, so the assets folder should only be added to the path when code is run
 # directly through Python
 assets_folder = '' if getattr(sys, 'frozen', False) else 'assets\\'
 assets_path = os.path.join(getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__))), assets_folder)
